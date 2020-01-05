cccc0a861c43126c9cdd44fe65afa2b512610a2f
elior8900 <elior.levi@gmail.com>
2020-01-05 12:40:24 +0300

    Updates

---
+++ precache-manifest.8e06954906df994122ce86000e05f7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f404307a8823ae77734c9ad23f8e3d1",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "f894bb859c9c099c595c",
+    "url": "/weather_app/static/css/main.43c8ff04.chunk.css"
+  },
+  {
+    "revision": "a071062e3d15969afcf3",
+    "url": "/weather_app/static/js/2.f4a19d33.chunk.js"
+  },
+  {
+    "revision": "f894bb859c9c099c595c",
+    "url": "/weather_app/static/js/main.e3f21305.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  }
+]);
